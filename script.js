f251f58150e1af45e4c4516143e88baba8c9df65
rudrarao28 <rudrarao1381@gmail.com>
2026-05-05 13:17:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("likeButton");
+
+button.addEventListener("click", function () {
+  alert("You liked this! ❤️");
+
+  // toggle class
+  button.classList.toggle("liked");
+
+  // change text
+  if (button.classList.contains("liked")) {
+    button.textContent = " Liked";
+  } else {
+    button.textContent = " Like";
+  }
+});
