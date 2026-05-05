d9f6598b5b49107c720381530e7debffeaddbddb
rudrarao28 <rudrarao1381@gmail.com>
2026-05-05 13:32:34 +0530

    Refactor like button event listener code

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const button = document.getElementById("likeButton");
 button.addEventListener("click", function () {
   alert("You liked this! ");
 
-  // toggle class
+  
   button.classList.toggle("liked");
 
-  // change text
+  
   if (button.classList.contains("liked")) {
     button.textContent = " Liked";
   } else {
